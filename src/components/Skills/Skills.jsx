ba8f5f888d568d85d338b681826d2e26e73caf33
DennisJoelW <dennisjoelw@gmail.com>
2024-05-12 12:37:17 +0700

    odiqwjdioq

--- src/components/Skills/Skills.jsx
+++ src/components/Skills/Skills.jsx
@@ -18,30 +18,30 @@ const Skills = () => {
 
          <ScrollAnimation animateIn='bounceInLeft' >
 
-         <h2 className=" block text-center md:text-5xl text-3xl uppercase font-semibold md:mb-14 mb-8">MY Skills</h2>
+          <h2 className=" block text-center md:text-5xl text-3xl uppercase font-semibold md:mb-14 mb-8">MY Skills</h2>
 
 
-            <div className=' flex md:flex-row flex-col w-full items-stretch md:gap-8 gap-5 justify-center'>
+              <div className=' flex md:flex-row flex-col w-full items-stretch md:gap-8 gap-5 justify-center'>
 
-                <div className='md:w-1/3 w-full py-16 px-10 bg-opacity-50 bg-black rounded-2xl'>
-                    <SiAndroidstudio size={68} className=' mb-4' />
-                    <h3 className='text-[#1ec8c8] md:text-2xl text-xl font-medium mb-4'>Android Development</h3>
-                    <p className='text-justify md:pr-5 text-[16px] '>Android Developer with internship experience using Java, familiar with kotlin. using MySQL for database management and Retrofit Library for HTTP Request. familiar with architectural patterns like MVVM and MVP.</p>
-                </div>
-                
-                <div className='md:w-1/3 w-full py-16 px-10  bg-opacity-50 bg-black rounded-2xl'>
-                    <RiCodeSSlashLine size={70} className=' mb-4'/>
-                    <h3 className='text-[#1ec8c8] md:text-2xl text-xl font-medium mb-4'>Front-End Development</h3>
-                    <p className='text-justify md:pr-5 text-[16px]'>Proficient in creating and designing dynamic web applications with React JS and Tailwind CSS to deliver modern front end design.</p>
-                </div>
+                  <div className='md:w-1/3 w-full py-16 px-10 bg-opacity-50 bg-black rounded-2xl'>
+                      <SiAndroidstudio size={68} className=' mb-4' />
+                      <h3 className='text-[#1ec8c8] md:text-2xl text-xl font-medium mb-4'>Android Development</h3>
+                      <p className='text-justify md:pr-5 text-[16px] '>Android Developer with internship experience using Java, familiar with Kotlin. using MySQL for database management and Retrofit Library for HTTP Request. familiar with architectural patterns like MVVM and MVP.</p>
+                  </div>
+                  
+                  <div className='md:w-1/3 w-full py-16 px-10  bg-opacity-50 bg-black rounded-2xl'>
+                      <RiCodeSSlashLine size={70} className=' mb-4'/>
+                      <h3 className='text-[#1ec8c8] md:text-2xl text-xl font-medium mb-4'>Front-End Development</h3>
+                      <p className='text-justify md:pr-5 text-[16px]'>Proficient in creating and designing dynamic web applications with React JS and Tailwind CSS to deliver modern front end design.</p>
+                  </div>
 
-                <div className='md:w-1/3 w-full py-16 px-10 bg-opacity-50 bg-black rounded-2xl'>
-                    <AiFillCode size={70} className=' mb-4' />
-                    <h3 className='text-[#1ec8c8] md:text-2xl text-xl font-medium mb-4'>Back-End Development</h3>
-                    <p className='text-justify md:pr-5 text-[16px]'>Experienced in developing  REST APIs with Express JS and Node JS, Proficient in database administration using MySQL and PostgreSQL.</p>
-                </div>
+                  <div className='md:w-1/3 w-full py-16 px-10 bg-opacity-50 bg-black rounded-2xl'>
+                      <AiFillCode size={70} className=' mb-4' />
+                      <h3 className='text-[#1ec8c8] md:text-2xl text-xl font-medium mb-4'>Back-End Development</h3>
+                      <p className='text-justify md:pr-5 text-[16px]'>Experienced in developing  REST APIs with Express JS and Node JS, Proficient in database administration using MySQL and PostgreSQL.</p>
+                  </div>
 
-            </div>
+              </div>
 
          </ScrollAnimation>
             
