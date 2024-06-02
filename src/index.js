baa2a171e87e9620105c41ece7a06f123c58e472
DennisJoelW <dennisjoelw@gmail.com>
2024-06-02 15:13:09 +0700

    final deploy

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import App from './App';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename='/portofolio'>
       <App />
     </BrowserRouter>
   </React.StrictMode>
